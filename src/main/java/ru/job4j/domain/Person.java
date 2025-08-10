e664d4e9e7b3d9d97c5d3ced19846ac33373464e
Maulen Zharylgasynov <exmpl@gmail.com>
2025-08-11 02:06:43 +0600

    Авторизация JWT

--- src/main/java/ru/job4j/domain/Person.java
+++ src/main/java/ru/job4j/domain/Person.java
@@ -1,12 +1,16 @@
 package ru.job4j.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Getter
+@Setter
 @Entity
 public class Person {
     @Id
